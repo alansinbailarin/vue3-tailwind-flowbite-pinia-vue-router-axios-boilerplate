e33199e0fe499cc848a24264f0823d4a53b6818c
Admin <admin@AlanPacheco.local>
2023-12-12 23:17:02 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>App</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Home page content</p>
+  </div>
+</template>
